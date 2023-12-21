5055e819ee6c6b127499b0b14238e6547c939fe4
Kaiji21 <elhaiba120@gmail.com>
2023-12-21 17:53:21 +0100

    Add Navbar component (Navbar.vue) Add Signin Modal component (Signinmodal.vue) Add Tailwind CSS to the project and configure it by initializing the necessary files, such as tailwind.config.js....

---
+++ Game-vue-Front/src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <fwb-navbar>
+        <template #logo>
+            <fwb-navbar-logo alt="Time Master Logo"
+                image-url="https://seeklogo.com/images/U/universite-cadi-ayyad-marrakech-maroc-logo-CCE5BB11B1-seeklogo.com.png"
+                link="#">
+                Time-Master
+            </fwb-navbar-logo>
+        </template>
+        <template #default="{ isShowMenu }">
+            <fwb-navbar-collapse :is-show-menu="isShowMenu">
+                <fwb-navbar-link is-active link="#">
+                    Home
+                </fwb-navbar-link>
+                <fwb-navbar-link link="#">
+                    Services
+                </fwb-navbar-link>
+                <fwb-navbar-link link="#">
+                    Contact
+                </fwb-navbar-link>
+            </fwb-navbar-collapse>
+        </template>
+        <template #right-side>
+            <fwb-button @click="showModal">
+                Get started
+            </fwb-button>
+        </template>
+    </fwb-navbar>
+    <Signinmodal :showModal="isShowModal" />
+</template>
+
+<script setup>
+import {
+    FwbButton,
+    FwbNavbar,
+    FwbNavbarCollapse,
+    FwbNavbarLink,
+    FwbNavbarLogo,
+} from 'flowbite-vue'
+import Signinmodal from './SigninModal.vue'
+import { ref } from 'vue'
+
+const isShowModal = ref(false);
+
+function showModal() {
+    isShowModal.value = !isShowModal.value;
+}
+
+
+</script>
---
+++ Game-vue-Front/src/components/SigninModal.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- <fwb-button @click="showModal">
+        Open modal
+    </fwb-button> -->
+
+    <fwb-modal v-if="internalShowModal" @close="closeModal">
+        <template #header>
+            <div class="flex items-center text-lg">
+                Terms of Service
+            </div>
+        </template>
+        <template #body>
+            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                companies around the world are updating their terms of service agreements to comply.
+            </p>
+            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
+                to ensure a common set of data rights in the European Union. It requires organizations to notify users as
+                soon as possible of high-risk data breaches that could personally affect them.
+            </p>
+        </template>
+        <template #footer>
+            <div class="flex justify-between">
+                <fwb-button @click="closeModal" color="alternative">
+                    Decline
+                </fwb-button>
+                <fwb-button @click="closeModal" color="green">
+                    I accept
+                </fwb-button>
+            </div>
+        </template>
+    </fwb-modal>
+</template>
+
+<script lang="ts" setup>
+import { ref,defineProps,watch  } from 'vue'
+import { FwbButton, FwbModal } from 'flowbite-vue'
+
+const props = defineProps(['showModal']);
+const internalShowModal = ref(false);
+
+function closeModal() {
+  internalShowModal.value = false;
+}
+
+watch(() => props.showModal, (newValue) => {
+  internalShowModal.value = newValue;
+});
+</script>
